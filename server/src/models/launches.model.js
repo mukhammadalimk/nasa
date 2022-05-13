d8ac95ea4fc19f6a764f95165ee45822091a2381
Mukhammadali <mukhammadalime@gmail.com>
2022-05-13 17:08:41 +0900

    Minimizing API Load

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -19,9 +19,8 @@ saveLaunch(launch);
 
 const SPACEX_API_URL = `https://api.spacexdata.com/v4/launches/query`;
 
-async function loadLaunchData() {
+async function populateLaunches() {
   console.log("Downloading launch data...");
-
   const response = await axios.post(SPACEX_API_URL, {
     query: {},
     options: {
@@ -47,6 +46,7 @@ async function loadLaunchData() {
 
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc.payloads;
+
     const customers = payloads.flatMap((payload) => {
       return payload.customers;
     });
@@ -62,11 +62,30 @@ async function loadLaunchData() {
     };
 
     console.log(`${launch.flightNumber} ${launch.mission}`);
+
+    // populate launches collection
+  }
+}
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: "Falcon 1",
+    mission: "FalconSat",
+  });
+
+  if (firstLaunch) {
+    console.log("Launch data already loaded");
+  } else {
+    await populateLaunches();
   }
 }
 
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter);
+}
+
 async function existsLaunchWithId(launchId) {
-  return await launchesDatabase.findOne({ flightNumber: launchId });
+  return await findLaunch({ flightNumber: launchId });
 }
 
 async function getLatestFlightNumber() {
