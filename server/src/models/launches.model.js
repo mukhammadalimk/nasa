cb39d8ee9673b08b07565aac97e0fd25c39c8910
Mukhammadali <mukhammadalime@gmail.com>
2022-05-13 20:34:52 +0900

    Sorting Paginated Data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -4,19 +4,6 @@ const planets = require("./planets.mongo");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100, // flight_number
-  mission: "Keplor Exploration X", // name
-  rocket: "Explorer IS1", // rocket.name
-  launchDate: new Date("December 27, 2030"), // date_local
-  target: "Kepler-442 b", // not applicable
-  customers: ["ZTM", "NASA"], //payloads.cutomers for each payload
-  upcoming: true, // upcoming
-  success: true, // success
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = `https://api.spacexdata.com/v4/launches/query`;
 
 async function populateLaunches() {
@@ -106,6 +93,7 @@ async function getLatestFlightNumber() {
 async function getAllLaunches(skip, limit) {
   return await launchesDatabase
     .find({}, { __v: 0, _id: 0 })
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
 }
